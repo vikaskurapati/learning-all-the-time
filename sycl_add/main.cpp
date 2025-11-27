20acd73ad3c7fd9be2f4d256585f2b27e292adc4
Vikas Kurapati <7347463+vikaskurapati@users.noreply.github.com>
2025-11-27 11:11:19 +0100

    Implement SYCL parallel computation with shared memory
    Initialize arrays A, B, and C, and allocate shared memory for the result. Perform a parallel operation to modify the result based on the secret string.

--- sycl_add/main.cpp
+++ sycl_add/main.cpp
@@ -1,1 +1,33 @@
+#include <sycl/sycl.hpp>
+#include <iostream>
+using namespace sycl;
 
+const std::string secret{
+"Ifmmp-!xpsme\"\012J(n!tpssz-!Ebwf/!"
+"J(n!bgsbje!J!dbo(u!ep!uibu/!.!IBM\01"};
+
+const auto sz = secret.size();
+
+#define VECTOR_SIZE 1000000
+
+int main(){
+  std::array<int, VECTOR_SIZE> A, B, C;
+  for(std::size_t i; i < VECTOR_SIZE; i++){
+    A[i] = 1;
+    B[i] = 2;
+    C[i] = 3;
+  }
+
+  std::cout << result << "\n";
+  queue q;
+  char* result = malloc_shared<char> (sz, q);
+  std::memcpy(result, secret.data(), sz);
+
+  q.parallel_for(sz, [=](auto& i){
+      result[i] -= 1;
+  }).wait();
+
+  std::cout << result << "\n";
+  free(result, q);
+  return 0;
+}
