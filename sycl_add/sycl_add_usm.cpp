29ab6f463040406f71cc60e2c9a12533aae9ef93
Vikas Kurapati <vikas.kurapati@tum.de>
2025-11-28 09:52:19 +0100

    trying to fix the logic

--- sycl_add/sycl_add_usm.cpp
+++ sycl_add/sycl_add_usm.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <chrono>
+#include <cmath>
 
 using namespace sycl;
 
@@ -42,9 +43,11 @@ int main(){
   q.wait();
 
   q.submit([&](handler &h){
-		  h.parallel_for(range{VECTOR_SIZE/1024, 1024}, [=](id<2> i){
+    size_t num_groups = (VECTOR_SIZE + 1023) / 1024;
+		  h.parallel_for(range{num_groups, 1024}, [=](id<2> i){
         size_t idx = i[0] * 1024 + i[1];
-        cDevice[idx] = aDevice[idx] + bDevice[idx];});
+        if(idx < VECTOR_SIZE)
+        {cDevice[idx] = aDevice[idx] + bDevice[idx];}});
 		  });
   q.wait();
   // write kernel code here
